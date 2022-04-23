c35a6a8b01bdd92d8c4c29bb55b79566d8485416
Rizky Fajri <mansamussolini@gmail.com>
2022-04-23 16:17:19 +0700

    Create Movie.cs

---
+++ modul8_1302204043/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace modul8_1302204043
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+    }
+}
