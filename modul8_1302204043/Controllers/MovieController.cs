400133db1112a934429ae49a63b987e3c7fa565c
Rizky Fajri <mansamussolini@gmail.com>
2022-04-23 17:12:24 +0700

    Create MovieController.cs

---
+++ modul8_1302204043/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul8_1302204043.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MovieController : Controller
+    {
+
+        [HttpGet(Name ="Movies")]
+        [HttpPost(Name ="Movies")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
